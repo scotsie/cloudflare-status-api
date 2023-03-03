b8089d67c3da71daf3bdc071ecdfb261a64d3b98
Scott Crace <scott.crace@momentumtelecom.com>
2023-03-02 21:44:24 -0500

    correcing variable reference in loop

--- local/lib/check_mk/base/plugins/agent_based/cloudflare_status_api.py
+++ local/lib/check_mk/base/plugins/agent_based/cloudflare_status_api.py
@@ -72,10 +72,10 @@ def check_cloudflare_status_api(item, section) -> CheckResult:
             if site["name"] == item:
                 output = f'{site["name"]}'
                 detail = ""
-                if site["components"]:
+                if site["components"] != "None":
                     # iterate through the subcomponents of the site and 
                     # add them as details if they are not operational.
-                    for subcomponent in component["components"]:
+                    for subcomponent in site["components"]:
                         res = list(filter(lambda section: section["id"] == subcomponent, section))
                         if res[0]["status"] != "operational":
                             detail += f'{res[0]["name"]}-{res[0]["status"]}\\n'
